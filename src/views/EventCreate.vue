1f2b26ce34f89fe542c7e7c7825197529af74f73
AninHuang <lifeanne19904@gmail.com>
2020-03-08 20:53:32 +0800

    Using mapGetters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,18 +3,16 @@
     <h1>Create an Event, {{ user.name }}</h1>
     <h1>{{ user.name }} is being used in here</h1>
     <h2>{{ user.id }}</h2>
-    <p>{{ getEvent(1) }}</p>
+    <p>{{ getEventById(2) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   computed: {
-    getEvent() {
-      return this.$store.getters.getEventById
-    },
+    ...mapGetters(['getEventById']),
     ...mapState(['user'])
   }
 }
