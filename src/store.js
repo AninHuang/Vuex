65be19bd1aaa7f2d4c2798b56e1c1aaaff1f4f32
AninHuang <lifeanne19904@gmail.com>
2020-03-08 20:47:18 +0800

    Using getters in getters

--- src/store.js
+++ src/store.js
@@ -6,13 +6,27 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: { id: 'testID123', name: 'Anin' },
-    categories: ['Technology', 'Sports', 'Media', 'Regional', 'Environmental']
+    categories: ['Technology', 'Sports', 'Media', 'Regional', 'Environmental'],
+    todos: [
+      { id: 1, text: 'to do 1', done: true },
+      { id: 2, text: 'to do 2', done: false },
+      { id: 3, text: 'to do 3', done: true }
+    ]
   },
   mutations: {},
   actions: {},
   getters: {
     catLength: state => {
       return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    unDoneTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
     }
+    // unDoneTodosCount: state => {
+    //   return state.todos.filter(todo => !todo.done)
+    // }
   }
 })
