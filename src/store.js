7e90b2ecf38a82b8692ca4f9c7d1bec8d1e84439
AninHuang <lifeanne19904@gmail.com>
2020-03-08 19:48:36 +0800

    Access the state by using $store in the component

--- src/store.js
+++ src/store.js
@@ -4,7 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'testID123', name: 'Anin' }
+  },
   mutations: {},
   actions: {}
 })
