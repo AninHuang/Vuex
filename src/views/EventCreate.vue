e5654c0c8f84a3db58aecc2a70a0c7c4eeef3a5b
AninHuang <lifeanne19904@gmail.com>
2020-03-08 19:59:09 +0800

    Access state in multiple places in component

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event, {{ $store.state.user.name }}</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <h1>{{ userName }} is being used in here</h1>
+    <h2>{{ userID }}</h2>
+  </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      userName() {
+        return this.$store.state.user.name
+      },
+      userID() {
+        return this.$store.state.user.id
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
