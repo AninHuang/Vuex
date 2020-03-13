da8e83167b1406efc3167a5b22984bffa6c0fb66
AninHuang <lifeanne19904@gmail.com>
2020-03-13 23:21:32 +0800

    Modify EventShow

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -32,11 +32,14 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
+
 export default {
   props: ['id'],
   created() {
     this.$store.dispatch('fetchEvent', this.id)
-  }
+  },
+  computed: mapState(['event'])
 }
 </script>
 <style scoped>
